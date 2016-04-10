9b0b6701d7cfa48279c5ca921a6e50d3e61360d8
phapli <phapli1991@gmail.com>
2016-04-10 22:14:36 +0700

    New Android Source

---
+++ dev/adroid/ise/app/src/main/java/com/phapli/ise/SectionsPagerAdapter.java
@@ -0,0 +1,53 @@
+package com.phapli.ise;
+
+/**
+ * Created by root on 10/04/2016.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return PlaceholderFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 3 total pages.
+        return 4;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Learn";
+            case 1:
+                return "Find";
+            case 2:
+                return "Analytics";
+            case 3:
+                return "Setting";
+        }
+        return null;
+    }
+}
